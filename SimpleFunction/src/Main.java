8e63a19e4d99e7f428bc633815709012a1cd4264
George0097 <151682370+George0097@users.noreply.github.com>
2024-04-28 15:56:37 +0300

    Add files via upload

---
+++ SimpleFunction/src/Main.java
@@ -0,0 +1,19 @@
+
+class A{
+
+    static void Method() {
+
+       String i = "Hello World";
+        System.out.println(i);
+
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        A.Method();
+    }
+}
