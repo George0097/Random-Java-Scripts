d7066b5591d59889f9119fc5af94b72d1e39e646
George0097 <151682370+George0097@users.noreply.github.com>
2024-05-05 23:09:28 +0300

    Add files via upload

---
+++ SimpleFunction/src/GetterSetterJava.java
@@ -0,0 +1,27 @@
+public class GetterSetterJava {
+    private int myNumber;
+
+    // Getter method for myNumber
+    public int getMyNumber() {
+        return myNumber;
+    }
+
+    // Setter method for myNumber
+    public void setMyNumber(int newValue) {
+        myNumber = newValue;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        GetterSetterJava obj = new GetterSetterJava();
+
+        // Set the value using the setter
+        obj.setMyNumber(50);
+
+        // Get the value using the getter
+        int value = obj.getMyNumber();
+        System.out.println(value);
+    }
+}
+
