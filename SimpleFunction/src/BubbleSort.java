03410c711692314cff01a497f2b818389514551a
George0097 <151682370+George0097@users.noreply.github.com>
2024-05-18 23:34:43 +0300

    Update BubbleSort.java

--- SimpleFunction/src/BubbleSort.java
+++ SimpleFunction/src/BubbleSort.java
@@ -11,4 +11,14 @@ public class BubbleSort {
                     arr[j + 1] = temp;
                 }
     }
+
+
+    public static void main(String args[]) {
+        BubbleSort bubble = new BubbleSort();
+        int arr[] = {95,234,625,114012,03210,128,256};
+        bubble.bubbleSort(arr);
+        System.out.print("Sorted: ");
+        for(int i=0; i<arr.length; i++)
+            System.out.print(arr[i] + "  ");
+    }
 }
